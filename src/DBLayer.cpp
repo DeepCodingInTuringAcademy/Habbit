08b7ec58242533179b55d72a6573002fa05deb8e
frozenlemontee <frozenlemontee@gmail.com>
2025-05-18 11:26:30 +0800

    refactor: Add default return

--- src/DBLayer.cpp
+++ src/DBLayer.cpp
@@ -2,6 +2,7 @@
 
 int DBLayer::dataBaseOpen(std::string&& db_file_name)
 {
+    return 0;
 }
 
 void DBLayer::dataBaseClose(sqlite3* db)
@@ -13,32 +14,40 @@ DBLayer::DBLayer(std::string&& db_file_name)
 
 std::vector<Habit> DBLayer::getHabitLists() const
 {
+    return std::vector<Habit>();
 }
 
 bool DBLayer::insertHabit(const Habit& habit)
 {
+    return false;
 }
 
 bool DBLayer::updateHabit(std::size_t habit_id)
 {
+    return false;
 }
 
 bool DBLayer::deleteHabit(std::size_t habit_id)
 {
+    return false;
 }
 
 std::vector<Event> DBLayer::getEventLists() const
 {
+    return std::vector<Event>();
 }
 
 bool DBLayer::insertEvent(const Event& event)
 {
+    return false;
 }
 
 bool DBLayer::updateEvent(std::size_t event_id)
 {
+    return false;
 }
 
 bool DBLayer::deleteEvent(std::size_t event_id)
 {
+    return false;
 }
