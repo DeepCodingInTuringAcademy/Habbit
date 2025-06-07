9d06d6bfe0c7b66bfcb8bd4bb6c11171cf4f14e0
FrozenlemonTee <1115306170@qq.com>
2025-06-07 19:45:15 +0800

    refactor: Add initialize of remind_time

--- src/Event.h
+++ src/Event.h
@@ -15,14 +15,15 @@ class Event
     bool is_expired_flag;
     bool is_deleted;
 
-    explicit Event(std::size_t event_id,
-                   std::size_t user_id,
-                   std::string  title,
-                   Date event_date,
-                   Time event_time,
-                   bool remind_flag,
-                   bool is_expired_flag,
-                   bool is_deleted);
+    explicit Event(std::size_t event_id = 0,
+                   std::size_t user_id = 0,
+                   std::string title = "",
+                   Date event_date = Date(),
+                   Time event_time = Time(),
+                   bool remind_flag = false,
+                   Time remind_time = Time(),
+                   bool is_expired_flag = false,
+                   bool is_deleted = false);
 };
 
 #endif //EVENT_H
