0795e4fba41d83c2685026979a425a988b0b306e
FrozenlemonTee <1115306170@qq.com>
2025-06-08 21:37:51 +0800

    refactor: Change to init list

--- src/Event.h
+++ src/Event.h
@@ -15,13 +15,13 @@ class Event
     bool is_expired_flag;
     bool is_deleted;
 
-    explicit Event(std::size_t event_id = 0,
+    explicit Event(std::size_t event_id = 0,  //NOLINT
                    std::size_t user_id = 0,
                    std::string title = "",
-                   Date event_date = Date(),
-                   Time event_time = Time(),
+                   Date event_date = {},
+                   Time event_time = {},
                    bool remind_flag = false,
-                   Time remind_time = Time(),
+                   Time remind_time = {},
                    bool is_expired_flag = false,
                    bool is_deleted = false);
 };
--- src/Habit.h
+++ src/Habit.h
@@ -18,8 +18,8 @@ struct Habit
                    std::size_t user_id = 0,
                    std::string name = "",
                    std::size_t target_count = 0,
-                   Date start_date = Date(),
-                   Date end_date = Date(),
+                   Date start_date = {},
+                   Date end_date = {},
                    bool is_active = false,
                    bool is_deleted = false);
 };
