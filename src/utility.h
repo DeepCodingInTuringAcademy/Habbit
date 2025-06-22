d4ba3f4060dd503ac9505a733469143c475d5469
FrozenlemonTee <1115306170@qq.com>
2025-06-22 20:57:10 +0800

    feat: Add split

--- src/utility.h
+++ src/utility.h
@@ -1,10 +1,13 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 #include <string>
+#include <vector>
 
 class utility
 {
 public:
+    static std::vector<std::string> split(const std::string &str, char delimiter);
+
     static std::string formatNumber(std::size_t num);
 };
 
