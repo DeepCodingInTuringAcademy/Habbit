13120bd27d4ef04f0fc25f46e9dc1c04a20c4dcb
FrozenlemonTee <1115306170@qq.com>
2025-06-22 20:59:57 +0800

    feat: to_string complete

--- src/Times.cpp
+++ src/Times.cpp
@@ -2,8 +2,14 @@
 
 std::string to_string(const Date& date)
 {
+    return std::to_string(static_cast<int>(date.year())) + "-"
+         + std::to_string(static_cast<unsigned int>(date.month())) + "-"
+         + std::to_string(static_cast<unsigned int>(date.day()));
 }
 
 std::string to_string(const Time& time)
 {
+    return std::to_string(time.hours().count()) + ":"
+         + std::to_string(time.minutes().count()) + ":"
+         + std::to_string(time.seconds().count());
 }
