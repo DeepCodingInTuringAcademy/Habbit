16b6dcf6fbf6a41c35ff87212d5e370a7f1d3705
Rain0832 <2710007824@qq.com>
2025-07-06 16:07:28 +0800

    style: correct code as standard

--- src/DateRecord.cpp
+++ src/DateRecord.cpp
@@ -1,6 +1,5 @@
 #include "DateRecord.h"
 
-
 DateRecord::DateRecord(
     const std::vector<std::pair<Time, Habit>>& habit_records,
     const std::vector<std::pair<Time, Pomodoro>>& pomodoro_records
