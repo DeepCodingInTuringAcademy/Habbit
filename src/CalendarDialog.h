128e109ac4d00d3c2fb768a5826ac78fccc57414
FrozenlemonTee <1115306170@qq.com>
2025-07-25 18:11:41 +0800

    feat: Modify CalendarDialog

--- src/CalendarDialog.h
+++ src/CalendarDialog.h
@@ -1,41 +1,43 @@
 #ifndef HABBIT_CALENDARDIALOG_H
 #define HABBIT_CALENDARDIALOG_H
 #include <QDialog>
-#include <QDate>
-#include <QPushButton>
-#include <QGridLayout>
 #include <QLabel>
 #include <QSpinBox>
 #include <QComboBox>
 
-class CalendarDialog : public QDialog
+#include "Calendar.h"
+#include "ServiceLayer.h"
+
+class CalendarDialog final : public QDialog
 {
     Q_OBJECT;
-
 public:
-    explicit CalendarDialog(QWidget *parent = nullptr);
-    [[nodiscard]] QDate selectedDate() const;
-    void setSelectedDate(const QDate &date);
+    explicit CalendarDialog(QWidget* parent = nullptr);
 
-private slots:
-    void updateCalendar();
-    void onDateClicked();
-    void onMonthChanged(int index);
-    void onYearChanged(int value);
-    void onPrevMonth();
-    void onNextMonth();
+    [[nodiscard]] QDate selectedDate() const;
+    void setSelectedDate(const QDate& date);
 
 private:
+    Calendar calendar_util;              ///< 日历工具类（非UI类）
+    QDate selected_date;                 ///< 当前选中日期
+    QDate current_month;                 ///< 当前显示的月份
+
+    QVBoxLayout* main_layout = nullptr;
+    QGridLayout* grid_layout = nullptr;
+
+    QLabel* month_year_label = nullptr;
+    QSpinBox* year_spin_box = nullptr;
+    QComboBox* month_combo_box = nullptr;
+
     void createHeader();
     void createCalendarGrid();
     void createFooter();
 
-    QDate current_date;
-    QGridLayout* grid_layout;
-    QLabel* month_year_label;
-    QSpinBox* year_spin_box;
-    QComboBox* month_combo_box;
-    QList<QPushButton*> date_buttons;
+private slots:
+    void onMonthChanged(int index);
+    void onYearChanged(int year);
+    void onPrevMonth();
+    void onNextMonth();
 };
 
 #endif //HABBIT_CALENDARDIALOG_H
