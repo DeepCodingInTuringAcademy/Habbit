4fa0bc14e26c0d6d3ae4e1dc9547c8ba0ff33b65
Rain0832 <2710007824@qq.com>
2025-07-26 00:00:44 +0800

    refactor: Set the window size as proper fixed size

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int testHabbitModule(int argc, char* argv[], const ViewLayer::ViewType type)
     QApplication app(argc, argv);
 
     ViewLayer view;
-    view.resize(1000, 800);
+    view.setFixedSize(1000, 800);
     view.setWindowTitle("Habbit 效率管理软件");
     view.setCurrentView(type);
     view.show();
