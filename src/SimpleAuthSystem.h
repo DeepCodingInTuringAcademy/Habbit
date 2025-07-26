18c7c7426ba646d6898ccf413c0f5e929024bf38
uiber3015 <202383015@uibe.edu.cn>
2025-07-26 22:54:38 +0800

    update login

---
+++ src/SimpleAuthSystem.h
@@ -0,0 +1,51 @@
+#ifndef SIMPLEAUTHSYSTEM_H
+#define SIMPLEAUTHSYSTEM_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QProcess>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <functional>
+
+class SimpleAuthSystem : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SimpleAuthSystem(QWidget *parent = nullptr);
+
+private slots:
+    void onLoginClicked();
+    void onRegisterClicked();
+    void onUpdateSettingsClicked();
+    void onLogoutClicked();
+
+private:
+    // UI组件
+    QLineEdit *usernameEdit;
+    QLineEdit *passwordEdit;
+    QLineEdit *emailEdit;
+    QLineEdit *nicknameEdit;
+    QLineEdit *skinEdit;
+    QPushButton *loginButton;
+    QPushButton *registerButton;
+    QPushButton *updateButton;
+    QPushButton *logoutButton;
+    QLabel *statusLabel;
+
+    // 存储数据
+    QString serverUrl;
+    QString accessToken;
+    int userId;
+
+    // 辅助函数：发送HTTP请求
+    void sendHttpRequest(const QString &endpoint, const QString &data,
+                         const QString &method, const QString &token,
+                         std::function<void(bool, const QJsonDocument &)> callback);
+};
+
+#endif // SIMPLEAUTHSYSTEM_H
