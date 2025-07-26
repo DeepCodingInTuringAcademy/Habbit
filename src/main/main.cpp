50c618bddf861088561891d7102897e773aa58cc
FrozenlemonTee <1115306170@qq.com>
2025-07-27 00:06:17 +0800

    test: Add test for login

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,4 +1,6 @@
 #include<QApplication>
+
+#include "SimpleAuthSystem.h"
 #include "ViewLayer.h"
 
 // 临时测试函数
@@ -13,6 +15,16 @@ int testHabbitModule(int argc, char* argv[], const ViewLayer::ViewType type)
     view.setWindowTitle("Habbit 效率管理软件");
     view.setCurrentView(type);
     view.show();
+
+    return app.exec();
+}
+
+int testHabbitModule(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    SimpleAuthSystem authSystem;
+    authSystem.show();
 
     return app.exec();
 }
