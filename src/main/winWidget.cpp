98165ee630526a1918bb737fb52cd23d25bdf81b
FrozenLemonTee <1115306170@qq.com>
2025-07-27 21:04:44 +0800

    refactor: Release 1.0

--- src/main/winWidget.cpp
+++ src/main/winWidget.cpp
@@ -1,8 +1,13 @@
 #include <QApplication>
+#include <windows.h>
 #include "CalendarWinWidget.h"
 
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+    ::ShowWindow(::GetConsoleWindow(), SW_HIDE);  // 隐藏控制台
+#endif
+
     QApplication a(argc, argv);
     CalendarWinWidget widget{nullptr, new ServiceLayer{}};
     widget.show();
