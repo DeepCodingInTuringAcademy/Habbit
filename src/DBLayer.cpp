326c2d5d7b0057744abe414043b2cb4c580fe7d1
FrozenlemonTee <1115306170@qq.com>
2025-08-18 17:31:58 +0800

    fix: Fix the database-related issues of the Pomodoro

--- src/DBLayer.cpp
+++ src/DBLayer.cpp
@@ -70,7 +70,8 @@ DBLayer::DBLayer(std::string db_file_name) : db_file_name_(std::move(db_file_nam
                       "userId INTEGER, "
                       "recordDate TEXT, "
                       "recordTime TEXT, "
-                      "record TEXT)";
+                      "recordDuration TEXT, "
+                      "recordMark TEXT)";
     if (!query.exec(pomoSql))
     {
         qDebug() << "PomodoroTable 创建失败：" << query.lastError().text();
@@ -428,22 +429,30 @@ void DBLayer::insertPomoRecord(Pomodoro pomo)
 {
     if (!openDatabase())
     {
-        qDebug() << "数据库打开失败，无法插入番茄钟记录";
+        qDebug() << "数据库打开失败：" << db_.lastError().text();
         return;
     }
-    Utility utility;
+
+    QDateTime currentDateTime = QDateTime::currentDateTime();
+    QString dateStr = currentDateTime.toString("yyyy-MM-dd");
+    QString timeStr = currentDateTime.toString("HH:mm:ss");
 
     QSqlQuery query(db_);
-    query.prepare("INSERT INTO PomodoroTable (userId, recordDate, recordTime, record) "
-                  "VALUES (:userId, :recordDate, :recordTime, :record)");
-    query.bindValue(":userId", 0); // 假设 userId 为 0
-    query.bindValue(":recordDate", QString::fromStdString(toString(utility.getCurrentTimeStamp().first)));
-    query.bindValue(":recordTime", QString::fromStdString(toString(pomo.pomodoro_time)));
-    query.bindValue(":record", QString::fromStdString(pomo.record));
+    query.prepare("INSERT INTO PomodoroTable (userId, recordDate, recordTime, recordDuration, recordMark) "
+                 "VALUES (:userId, :recordDate, :recordTime, :recordDuration, :recordMark)");
+
+    // 确保所有参数都正确绑定
+    query.bindValue(":userId", getCurrentUserID());
+    query.bindValue(":recordDate", dateStr);
+    query.bindValue(":recordTime", timeStr);
+    query.bindValue(":recordDuration", QString::fromStdString(toString(pomo.pomodoro_duration)));
+    query.bindValue(":recordMark", QString::fromStdString(pomo.record));
 
     if (!query.exec())
     {
         qDebug() << "插入番茄钟记录失败：" << query.lastError().text();
+        qDebug() << "执行的SQL:" << query.lastQuery();
+        qDebug() << "绑定的值:" << query.boundValues();
     }
     closeDatabase();
 }
@@ -492,28 +501,28 @@ DateRecord DBLayer::getRecordbyDate(Date date) const
     }
 
     // 查询指定日期的事项记录
-    QSqlQuery eventQuery(db_);
-    eventQuery.prepare("SELECT * FROM EventTable WHERE eventDate = :date AND isDeleted = 0");
-    eventQuery.bindValue(":date", dateStr);
+    QSqlQuery event_query(db_);
+    event_query.prepare("SELECT * FROM EventTable WHERE eventDate = :date AND isDeleted = 0");
+    event_query.bindValue(":date", dateStr);
 
-    if (!eventQuery.exec())
+    if (!event_query.exec())
     {
-        qDebug() << "查询指定日期的事项记录失败：" << eventQuery.lastError().text();
+        qDebug() << "查询指定日期的事项记录失败：" << event_query.lastError().text();
     }
     else
     {
-        while (eventQuery.next())
+        while (event_query.next())
         {
             Event event{
-                eventQuery.value("eventId").toULongLong(),
-                eventQuery.value("userId").toULongLong(),
-                eventQuery.value("title").toString().toStdString(),
-                dateFromString(eventQuery.value("eventDate").toString().toStdString()),
-                timeFromString(eventQuery.value("eventTime").toString().toStdString()),
-                eventQuery.value("remindFlag").toBool(),
-                timeFromString(eventQuery.value("remindTime").toString().toStdString()),
-                eventQuery.value("isExpiredFlag").toBool(),
-                eventQuery.value("isDeleted").toBool()};
+                event_query.value("eventId").toULongLong(),
+                event_query.value("userId").toULongLong(),
+                event_query.value("title").toString().toStdString(),
+                dateFromString(event_query.value("eventDate").toString().toStdString()),
+                timeFromString(event_query.value("eventTime").toString().toStdString()),
+                event_query.value("remindFlag").toBool(),
+                timeFromString(event_query.value("remindTime").toString().toStdString()),
+                event_query.value("isExpiredFlag").toBool(),
+                event_query.value("isDeleted").toBool()};
 
             // 使用事项的时间作为记录时间
             Time time = event.event_time;
@@ -522,30 +531,32 @@ DateRecord DBLayer::getRecordbyDate(Date date) const
     }
 
     // 查询指定日期的番茄钟使用记录
-    QSqlQuery pomodoroQuery(db_);
-    pomodoroQuery.prepare("SELECT * FROM PomodoroTable WHERE recordDate = :date");
-    pomodoroQuery.bindValue(":date", dateStr);
+    QSqlQuery pomodoro_query(db_);
+    pomodoro_query.prepare("SELECT * FROM PomodoroTable WHERE recordDate = :date");
+    pomodoro_query.bindValue(":date", dateStr);
 
-    if (!pomodoroQuery.exec())
+    if (!pomodoro_query.exec())
     {
-        qDebug() << "查询指定日期的番茄钟使用记录失败：" << pomodoroQuery.lastError().text();
+        qDebug() << "查询指定日期的番茄钟使用记录失败：" << pomodoro_query.lastError().text();
     }
     else
     {
-        while (pomodoroQuery.next())
+        while (pomodoro_query.next())
         {
             // 提取数据库中的字段
-            std::size_t id = pomodoroQuery.value("pomoId").toULongLong();
-            QString recordTimeStr = pomodoroQuery.value("recordTime").toString();
-            std::string record = pomodoroQuery.value("record").toString().toStdString();
+            std::size_t id = pomodoro_query.value("pomoId").toULongLong();
+            QString record_end_time = pomodoro_query.value("recordTime").toString();
+            QString record_duration_str = pomodoro_query.value("recordDuration").toString();
+            std::string record = pomodoro_query.value("recordMark").toString().toStdString();
 
             // 将时间字符串转换为 Time 对象
-            Time time = timeFromString(recordTimeStr.toStdString());
+            Time duration = timeFromString(record_duration_str.toStdString());
+            Time end_time = timeFromString(record_end_time.toStdString());
 
             // 构造 Pomodoro 对象
-            Pomodoro pomodoro(id, time, record);
+            Pomodoro pomodoro(id, duration, record);
 
-            pomodoro_records.emplace_back(time, pomodoro);
+            pomodoro_records.emplace_back(end_time, pomodoro);
         }
     }
 
@@ -747,28 +758,13 @@ std::size_t DBLayer::getCurrentUserID() const
 {
     std::size_t currentUserId = 0;
 
-    if (!openDatabase())
-    {
-        qDebug() << "数据库打开失败，无法获取当前用户ID";
-        return currentUserId;
-    }
-
     QSqlQuery query(db_);
     query.prepare("SELECT userId FROM UserSettingsTable WHERE isLoggedIn = 1 LIMIT 1");
 
-    if (!query.exec())
-    {
-        qDebug() << "查询当前用户ID失败：" << query.lastError().text();
-        closeDatabase();
-        return currentUserId;
-    }
-
-    if (query.next())
-    {
+    if (query.exec() && query.next()) {
         currentUserId = query.value("userId").toULongLong();
     }
 
-    closeDatabase();
     return currentUserId;
 }
 
